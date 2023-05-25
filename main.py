7b7244ab4b4e7b11cd6bb6d3eaf7b1de11945621
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-05-26 00:50:40 +0300

    working with rows in a dataframe

--- main.py
+++ main.py
@@ -20,11 +20,24 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+
+# accessing series or columns using pandas
 temp_list = data["temp"].to_list()
 
 # data discovery phase of EDA
 avg_temp = round(data["temp"].mean(), 2)
-print("The average temperature is: ", avg_temp)
+# print("The average temperature is: ", avg_temp)
 
 max_temp = data["temp"].max()
-print("The max temperature is: ", max_temp)
+# print("The max temperature is: ", max_temp)
+
+# Using masks to pull entire rows from data frames
+
+monday = data[data["day"] == "Monday"]
+# print(monday)
+
+hottest_day_mask = data["temp"] == data["temp"].max()
+# print(data[hottest_day_mask])
+
+# accessing items of dataframe object created using mask eg monday
+print("Monday temperature in fahrenheit was ", int(monday.temp) * 9/5 + 32)
