b0fdf21e5aea3be7f7b0036ff20cb22c8a40ca6c
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-05-25 23:29:25 +0300

    different ways to work with csv files using with keyword and open, csv library and pandas.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import requests
+# place the csv file content into a list named data
+#
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#
+# print(data)
+
+# Create a list using csv library
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = [int(temp[1]) for temp in data if temp[1] != "temp"]
+#
+#     print(temperature)
+
+# Using pandas library, the most effective and shortest way of working with csv files
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
